a795d221d2e8df0b8c3aabe2ed4adaab1f0313f7
AlessandroSalogni <20013907@studenti.uniupo.it>
2020-07-07 21:32:42 +0200

    Sistemato codice per caricare dati su DB

--- venv/Src/XMLReader.py
+++ venv/Src/XMLReader.py
@@ -1,22 +1,29 @@
 from xml.dom import minidom
 # noinspection PyUnresolvedReferences
-import MongoDBConnection
+from DBConnection.MongoDBConnection import MongoDB
+import glob, os
 
-patientsFile = minidom.parse('../Resources/4.2.2. Navarra Files/Patients.XML')
-legend = []
+with MongoDB() as mongo:
+    for filePath in glob.glob("../Resources/4.2.2. Navarra Files/*.XML"):
+        filename = os.path.splitext(os.path.basename(filePath))[0].replace(" ", "_")
 
-patients = patientsFile.getElementsByTagName('Row')
+        xmlFile = minidom.parse(filePath)
+        rows = xmlFile.getElementsByTagName('Row')
 
-with MongoDBConnection.MongoDB() as mongo:
-    for patient in patients:
-        details = patient.getElementsByTagName('Cell')
+        legend = []
+        documents = []
+        i = 0
+        for row in rows:
+            details = row.getElementsByTagName('Cell')
 
-        if not legend:
-            for detail in details:
-                legend.append(detail.getElementsByTagName('Data')[0].firstChild.data)
-        else:
-            row = {}
-            for detail, col in zip(details, legend):
-                data = detail.getElementsByTagName('Data')[0].firstChild.data
-                row[col] = data
-            mongo.insert("patients", row)
+            if not legend:
+                legend = [detail.getElementsByTagName('Data')[0].firstChild.data for detail in details]
+            else:
+                documents.append({col: detail.getElementsByTagName('Data')[0].firstChild.data for detail, col in zip(details, legend)})
+
+                i += 1
+                if(i % 10000 == 0):
+                    mongo.insert(filename, documents, many=True)
+                    documents = []
+
+        mongo.insert(filename, documents, many=True)
