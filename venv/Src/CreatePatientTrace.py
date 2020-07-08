faace32d1c0e0bbf0183756065625644faa150ef
perottiriccardo <20013787@studenti.uniupo.it>
2020-07-08 15:01:17 +0200

    Escluso codice paziente dentro appointment

--- venv/Src/CreatePatientTrace.py
+++ venv/Src/CreatePatientTrace.py
@@ -8,7 +8,7 @@ with MongoDB() as mongo:
     for patient in mongo.query("Patients", query = {}, projection = {'Pac_Unif_Cod': 1, 'Age': 1, 'Gender': 1}):
         patient['appointments'] = []
 
-        for appointment in mongo.query("Appointment_Data", query = {'Pac_Unif_Cod': patient['Pac_Unif_Cod']}, projection = {'_id': 0, 'Month': 0, 'Week day': 0, 'Hour': 0}):
+        for appointment in mongo.query("Appointment_Data", query = {'Pac_Unif_Cod': patient['Pac_Unif_Cod']}, projection = {'_id': 0, 'Month': 0, 'Week day': 0, 'Hour': 0, 'Pac_Unif_Cod': 0}):
             try:
                 appointment['Visit day'] = datetime.strptime(appointment['Visit day'], "%Y-%m-%dT%H:%M:%S.%f").timestamp() * 1000
                 appointment['Waiting list entry date'] = datetime.strptime(appointment['Waiting list entry date'], "%Y-%m-%dT%H:%M:%S.%f").timestamp() * 1000
