03219ea2a7409f6a23990274fa367e05a4800e81
perottiriccardo <20013787@studenti.uniupo.it>
2020-07-13 10:22:29 +0200

    Altre statistiche su apppuntamenti

--- venv/Src/CreateAppointmentStatistics.py
+++ venv/Src/CreateAppointmentStatistics.py
@@ -11,8 +11,40 @@ def countDocs(mongoInstance, collectionName, parameters):
 with MongoDB() as mongo:
     visitTypes = ["First", "Revision", "Non-presential", "Special"]
     visitStatus = ["NoShowUp", "Cancelled HS", "Cancelled Pat", "Done", "Other"]
+    visitCharacters = ["Ordinary", "Preferential","Non-presential", "Results", "Extra"]
+    appointmentOrigins = ["Family doctor", "Specialist doctor", "Other"]
+    appointmentReminders = ["Phone", "SMS", "Phone+SMS", "Other", "None"]
 
     for vstat in visitStatus:
+        print("-----------------------")
         for vtype in visitTypes:
             print(vstat + " at " + vtype + ": " +
                   str(countDocs(mongo, "Appointment_Data", {"Visit type": vtype, "Visit status": vstat})) + "%")
+
+    print("-----------------------")
+    print("-----------------------")
+
+    for vstat in visitStatus:
+        print("-----------------------")
+        for rem in appointmentReminders:
+            print(vstat + " with reminder " + rem + ": " +
+                  str(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem, "Visit status": vstat})) + "%")
+
+    print("-----------------------")
+    print("-----------------------")
+
+    for vstat in visitStatus:
+        print("-----------------------")
+        for origin in appointmentOrigins:
+            print(vstat + " with appointment origin " + origin + ": " +
+                  str(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin, "Visit status": vstat})) + "%")
+
+    print("-----------------------")
+    print("-----------------------")
+
+    for vstat in visitStatus:
+        print("-----------------------")
+        for character in visitCharacters:
+            print(vstat + " with visit character " + character + ": " +
+                  str(countDocs(mongo, "Appointment_Data",
+                                {"Character of visit": character, "Visit status": vstat})) + "%")
