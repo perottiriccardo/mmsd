25b326bfe52a3d9c77d7a820acb7a7cccfd90a48
perottiriccardo <20013787@studenti.uniupo.it>
2020-07-15 10:14:27 +0200

    Altre statistiche su apppuntamenti

--- venv/Src/CreateAppointmentStatistics.py
+++ venv/Src/CreateAppointmentStatistics.py
@@ -15,36 +15,62 @@ with MongoDB() as mongo:
     appointmentOrigins = ["Family doctor", "Specialist doctor", "Other"]
     appointmentReminders = ["Phone", "SMS", "Phone+SMS", "Other", "None"]
 
+    for vtype in visitTypes:
+        docsRate = round(countDocs(mongo, "Appointment_Data", {"Visit type": vtype}), 3)
+        print("% " + vtype + ": " + str(docsRate))
+    print("-----------------------\n-----------------------")
+
+    for rem in appointmentReminders:
+        docsRate = round(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem}), 3)
+        print("% " + rem + ": " + str(docsRate))
+    print("-----------------------\n-----------------------")
+
+    for origin in appointmentOrigins:
+        docsRate = round(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin}), 3)
+        print("% " + origin + ": " + str(docsRate))
+    print("-----------------------\n-----------------------")
+
+    for character in visitCharacters:
+        docsRate = round(countDocs(mongo, "Appointment_Data", {"Character of visit": character}), 3)
+        print("% " + character + ": " + str(docsRate))
+    print("-----------------------\n-----------------------")
+
     for vstat in visitStatus:
-        print("-----------------------")
+        sum = 0
         for vtype in visitTypes:
+            docsRate = round(countDocs(mongo, "Appointment_Data", {"Visit type": vtype, "Visit status": vstat}), 3)
             print(vstat + " at " + vtype + ": " +
-                  str(countDocs(mongo, "Appointment_Data", {"Visit type": vtype, "Visit status": vstat})) + "%")
-
-    print("-----------------------")
-    print("-----------------------")
+                  str(docsRate) + "%")
+            sum += docsRate
+        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
+    print("-----------------------\n-----------------------")
 
     for vstat in visitStatus:
-        print("-----------------------")
+        sum = 0
         for rem in appointmentReminders:
-            print(vstat + " with reminder " + rem + ": " +
-                  str(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem, "Visit status": vstat})) + "%")
-
-    print("-----------------------")
-    print("-----------------------")
+            docsRate = round(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem, "Visit status": vstat}),3)
+            print(vstat + " with appointment remainder " + rem + ": " +
+                  str(docsRate) + "%")
+            sum += docsRate
+        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
+    print("-----------------------\n-----------------------")
 
     for vstat in visitStatus:
-        print("-----------------------")
+        sum = 0
         for origin in appointmentOrigins:
-            print(vstat + " with appointment origin " + origin + ": " +
-                  str(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin, "Visit status": vstat})) + "%")
+            docsRate = round(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin, "Visit status": vstat}),3)
+            print(vstat + " with origin " + origin + ": " +
+                  str(docsRate) + "%")
+            sum += docsRate
+        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
 
-    print("-----------------------")
-    print("-----------------------")
+    print("-----------------------\n-----------------------")
 
     for vstat in visitStatus:
-        print("-----------------------")
+        sum = 0
         for character in visitCharacters:
+            docsRate = round(countDocs(mongo, "Appointment_Data", {"Character of visit": character, "Visit status": vstat}),3)
             print(vstat + " with visit character " + character + ": " +
-                  str(countDocs(mongo, "Appointment_Data",
-                                {"Character of visit": character, "Visit status": vstat})) + "%")
+                  str(docsRate) + "%")
+            sum += docsRate
+        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
