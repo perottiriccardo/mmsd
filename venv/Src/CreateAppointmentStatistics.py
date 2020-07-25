cb2be9d21af228b9518a54b93201d3426d7dfcb2
perottiriccardo <20013787@studenti.uniupo.it>
2020-07-25 13:09:00 +0200

    Adattamento calcolo numero orari contemporanei servendosi degli arrotondamenti in patientTrace

--- venv/Src/CreateAppointmentStatistics.py
+++ venv/Src/CreateAppointmentStatistics.py
@@ -44,93 +44,94 @@ with MongoDB() as mongo:
 
     #Conta il numero di appuntamenti contemporanei per determinare il numero di dottori
     for i in range(1,17):
-        groups = mongo.db["Appointment_Data"].aggregate([
+        groups = mongo.db["PatientTrace"].aggregate([
+            {"$unwind": "$appointments"},
             {"$match":
                 {
                     "$and": [
-                        {"$or": [{"Visit status": "Done"}, {"Visit status": "NoShowUp"}]},
-                        {"Visit day": {"$not": {"$regex": ".*T00:00:00.*"}}}
+                        {"$or": [{"appointments.Visit status": "Done"}, {"appointments.Visit status": "NoShowUp"}]},
+                        {"appointments.Visit day": {"$not": {"$regex": ".*T00:00:00.*"}}}
                     ]
                 }
             },
-            {"$group": {"_id": "$Visit day", "total": {"$sum": 1}}},
+            {"$group": {"_id": "$appointments.Visit day", "total": {"$sum": 1}}},
             {"$sort": {"total": 1}}
         ])
 
         print(f"{i} - {sum(el['total'] == i for el in groups)}")
 
-    print(f"Appointment per day mean: {statistics.mean([g['total'] for g in appointmentPerDay(mongo)])}")
-    #Considerando slot di diversi minutaggi
-    slots = [5,10,15,20,25,30]
-    for slot in slots:
-        print(f"{slot} minutes slot - {math.ceil(doctorsPerSlotMean(mongo, slot, 8, 21))} doctors")
-
-    #Data fissata per riscontro slot durata
-    # for a in mongo.query("Appointment_Data", query={ "Visit day" : {"$regex" : "2017-03-21.*"}}, projection={"Visit day": 1}).sort("Visit day", 1):
-    #     print(a["Visit day"][11:])
-
-    days = ["Mond", "Tuesd", "Wedn", "Thursd", "Friday"]
-    for day in days:
-        print(
-            f"Mean doctors value for each week day"
-            f"{statistics.mean([g['total'] / (((21 - 8) * 60) / 15) for g in appointmentPerWeekDay(mongo, day)])}")
-
-    for vtype in visitTypes:
-        docsRate = round(countDocs(mongo, "Appointment_Data", {"Visit type": vtype}), 3)
-        print("% " + vtype + ": " + str(docsRate))
-    print("-----------------------\n-----------------------")
-
-    for rem in appointmentReminders:
-        docsRate = round(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem}), 3)
-        print("% " + rem + ": " + str(docsRate))
-    print("-----------------------\n-----------------------")
-
-    for origin in appointmentOrigins:
-        docsRate = round(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin}), 3)
-        print("% " + origin + ": " + str(docsRate))
-    print("-----------------------\n-----------------------")
-
-    for character in visitCharacters:
-        docsRate = round(countDocs(mongo, "Appointment_Data", {"Character of visit": character}), 3)
-        print("% " + character + ": " + str(docsRate))
-    print("-----------------------\n-----------------------")
-
-    for vstat in visitStatus:
-        sum = 0
-        for vtype in visitTypes:
-            docsRate = round(countDocs(mongo, "Appointment_Data", {"Visit type": vtype, "Visit status": vstat}), 3)
-            print(vstat + " at " + vtype + ": " +
-                  str(docsRate) + "%")
-            sum += docsRate
-        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
-    print("-----------------------\n-----------------------")
-
-    for vstat in visitStatus:
-        sum = 0
-        for rem in appointmentReminders:
-            docsRate = round(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem, "Visit status": vstat}),3)
-            print(vstat + " with appointment remainder " + rem + ": " +
-                  str(docsRate) + "%")
-            sum += docsRate
-        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
-    print("-----------------------\n-----------------------")
-
-    for vstat in visitStatus:
-        sum = 0
-        for origin in appointmentOrigins:
-            docsRate = round(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin, "Visit status": vstat}),3)
-            print(vstat + " with origin " + origin + ": " +
-                  str(docsRate) + "%")
-            sum += docsRate
-        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
-
-    print("-----------------------\n-----------------------")
-
-    for vstat in visitStatus:
-        sum = 0
-        for character in visitCharacters:
-            docsRate = round(countDocs(mongo, "Appointment_Data", {"Character of visit": character, "Visit status": vstat}),3)
-            print(vstat + " with visit character " + character + ": " +
-                  str(docsRate) + "%")
-            sum += docsRate
-        print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
+    # print(f"Appointment per day mean: {statistics.mean([g['total'] for g in appointmentPerDay(mongo)])}")
+    # #Considerando slot di diversi minutaggi
+    # slots = [5,10,15,20,25,30]
+    # for slot in slots:
+    #     print(f"{slot} minutes slot - {math.ceil(doctorsPerSlotMean(mongo, slot, 8, 21))} doctors")
+    #
+    # #Data fissata per riscontro slot durata
+    # # for a in mongo.query("Appointment_Data", query={ "Visit day" : {"$regex" : "2017-03-21.*"}}, projection={"Visit day": 1}).sort("Visit day", 1):
+    # #     print(a["Visit day"][11:])
+    #
+    # days = ["Mond", "Tuesd", "Wedn", "Thursd", "Friday"]
+    # for day in days:
+    #     print(
+    #         f"Mean doctors value for each week day"
+    #         f"{statistics.mean([g['total'] / (((21 - 8) * 60) / 15) for g in appointmentPerWeekDay(mongo, day)])}")
+    #
+    # for vtype in visitTypes:
+    #     docsRate = round(countDocs(mongo, "Appointment_Data", {"Visit type": vtype}), 3)
+    #     print("% " + vtype + ": " + str(docsRate))
+    # print("-----------------------\n-----------------------")
+    #
+    # for rem in appointmentReminders:
+    #     docsRate = round(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem}), 3)
+    #     print("% " + rem + ": " + str(docsRate))
+    # print("-----------------------\n-----------------------")
+    #
+    # for origin in appointmentOrigins:
+    #     docsRate = round(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin}), 3)
+    #     print("% " + origin + ": " + str(docsRate))
+    # print("-----------------------\n-----------------------")
+    #
+    # for character in visitCharacters:
+    #     docsRate = round(countDocs(mongo, "Appointment_Data", {"Character of visit": character}), 3)
+    #     print("% " + character + ": " + str(docsRate))
+    # print("-----------------------\n-----------------------")
+    #
+    # for vstat in visitStatus:
+    #     sum = 0
+    #     for vtype in visitTypes:
+    #         docsRate = round(countDocs(mongo, "Appointment_Data", {"Visit type": vtype, "Visit status": vstat}), 3)
+    #         print(vstat + " at " + vtype + ": " +
+    #               str(docsRate) + "%")
+    #         sum += docsRate
+    #     print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
+    # print("-----------------------\n-----------------------")
+    #
+    # for vstat in visitStatus:
+    #     sum = 0
+    #     for rem in appointmentReminders:
+    #         docsRate = round(countDocs(mongo, "Appointment_Data", {"Appointment remainder": rem, "Visit status": vstat}),3)
+    #         print(vstat + " with appointment remainder " + rem + ": " +
+    #               str(docsRate) + "%")
+    #         sum += docsRate
+    #     print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
+    # print("-----------------------\n-----------------------")
+    #
+    # for vstat in visitStatus:
+    #     sum = 0
+    #     for origin in appointmentOrigins:
+    #         docsRate = round(countDocs(mongo, "Appointment_Data", {"Origin of appointment": origin, "Visit status": vstat}),3)
+    #         print(vstat + " with origin " + origin + ": " +
+    #               str(docsRate) + "%")
+    #         sum += docsRate
+    #     print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
+    #
+    # print("-----------------------\n-----------------------")
+    #
+    # for vstat in visitStatus:
+    #     sum = 0
+    #     for character in visitCharacters:
+    #         docsRate = round(countDocs(mongo, "Appointment_Data", {"Character of visit": character, "Visit status": vstat}),3)
+    #         print(vstat + " with visit character " + character + ": " +
+    #               str(docsRate) + "%")
+    #         sum += docsRate
+    #     print("----------------------- Tot: " + str(round(sum,3)) + " %-----------------------")
