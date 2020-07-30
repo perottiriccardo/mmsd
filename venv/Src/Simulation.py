072917355f09082d8300209bc46c41aaac6a116f
perottiriccardo <20013787@studenti.uniupo.it>
2020-07-30 11:43:27 +0200

    Tuning completato

--- venv/Src/Simulation.py
+++ venv/Src/Simulation.py
@@ -1,7 +1,6 @@
 # noinspection PyUnresolvedReferences
 from DBConnection.MongoDBConnection import MongoDB
 import salabim as sim
-import numpy as np
 import configparser
 import math
 import image
@@ -58,7 +57,7 @@ class Appointment(sim.Component):
             yield self.hold(self.info['relative_visit_day'] - env.now())
 
             if str(env.now())[-2:] == ".0":
-                yield self.hold(sim.Uniform(14, 19, "hours"))
+                yield self.hold(sim.Uniform(8, 18, "hours"))
 
             # Richiedo una risorsa slot
             yield self.request(slots)
@@ -85,7 +84,6 @@ class Appointment(sim.Component):
 
 class DepartmentCapacity(sim.Component):
     def process(self):
-        exceedRequesters = 0
         while True:
             if int(math.ceil(env.now())) % 7 == 0 or int(math.ceil(env.now())) % 7 == 6:
                 slots.set_capacity(0)
@@ -98,19 +96,15 @@ class DepartmentCapacity(sim.Component):
 
                 slots.set_capacity(11)
                 doctors.set_capacity(11)
-                yield self.hold(env.hours(6))
+                yield self.hold(env.hours(6.5))
 
-                exceedRequesters += len(slots.requesters())
-                print(f"{int(env.now())} -> {len(slots.requesters())} -> total {exceedRequesters}")
-                slots.set_capacity(3)
-                doctors.set_capacity(3)
-                yield self.hold(env.hours(8))
+                slots.set_capacity(4)
+                doctors.set_capacity(4)
+                yield self.hold(env.hours(7.5))
 
                 slots.set_capacity(0)
                 doctors.set_capacity(0)
                 yield self.hold(env.hours(2))
-
-
 
 
 nAppointments = 0
@@ -121,11 +115,9 @@ cancelled = 0
 # config = configparser.ConfigParser()
 # config.read('ConfigFile.properties')
 
-timeSlot = 20
-#for ts in (5,10,15,20):
+timeSlot = 15
 env = sim.Environment(trace=False, time_unit='days')
 env.animate(True)
-#timeSlot = ts
 PatientGenerator()
 # Creata la risorsa slot con una capacità di 6
 slots = sim.Resource('Slot', capacity=11)
@@ -135,7 +127,7 @@ DepartmentCapacity()
 
 env.modelname("Patients not show up simulation")
 env.background_color("20%gray")
-env.speed(0.4)
+env.speed(1000)
 
 sim.AnimateQueue(slots.requesters(), x=30, y=650, title='Requester queue', direction='e', id='blue')
 sim.AnimateQueue(slots.claimers(), x=30, y=580, title='Claimers queue', direction='e', id='blue')
